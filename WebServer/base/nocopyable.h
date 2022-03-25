1004d83723f2b7b1bf362c5dbc8fa581880cf911
shikeqin123 <2509154582@qq.com>
2022-03-25 20:41:55 +0800

    add nocopyable

---
+++ WebServer/base/nocopyable.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//不可复制的基类
+class nocopyable{
+protected:
+    nocopyable(){};
+    ~nocopyable(){};
+private:
+    nocopyable(const nocopyable &);
+    const nocopyable& operator =(const nocopyable &)；
+}
