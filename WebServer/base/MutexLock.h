7301573e73c5f87a86528534bf85fc200c1e63f9
shikeqin123 <2509154582@qq.com>
2022-03-26 15:41:07 +0800

    add MutexLock

---
+++ WebServer/base/MutexLock.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include<pthread.h>
+#include"Condition.h"
+#include"nocopyable.h"
+
+class MutexLock:nocopyable{
+public:
+    MutexLock(){ pthread_mutex_init(&mutex_, NULL);}
+    ~MutexLock(){
+        pthread_mutex_lock(&mutex_);
+        pthread_mutex_destroy(&mutex_);
+    }
+    void lock(){ pthread_mutex_lock(&mutex_);}
+    void unlock(){ pthread_mutex_unlock(&mutex_);}
+  // 友元类不受访问权限影响
+private:
+    pthread_mutex_t* get(){ return &mutex_;};
+    friend class Condition;
+private:
+    pthread_mutex_t mutex_;
+
+};
+
+class MutexLockGuard:nocopyable{
+public:
+    MutexLockGuard(MutexLock &mutexLock):mutexLock_(mutexLock){
+        mutexLock_.lock();
+    }
+    ~MutexLockGuard(){
+        mutexLock_.unlock();
+    }
+private:
+    MutexLock &mutexLock_;
+};
