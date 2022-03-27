3570c7f8efa90b246383f8a9c59852255a4035ce
shikeqin123 <2509154582@qq.com>
2022-03-27 20:41:38 +0800

    Logging

--- WebServer/base/Logging.h
+++ WebServer/base/Logging.h
@@ -5,3 +5,25 @@
 #include <string>
 #include "LogStream.h"
 
+class Logger{
+public:
+    Logger(const char*fileName, int line);
+    ~Logger();
+    LogStream &stream(){ return impl_.stream_;}
+
+    static void setLogFileName(std::string fileName) { logFileName_ = fileName; }
+    static std::string getLogFileName() { return logFileName_; }
+private:
+    class Impl{
+        public:
+        Impl(const char* fileName, int line);
+        void formatTime();
+        LogStream stream_;
+        int line_;
+        std::string basename_;
+    };
+    Impl impl_;
+    static std::string logFileName_;
+};
+
+#define LOG Logger(__FILE__, __LINE__).stream()
